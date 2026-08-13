2d11b05b83fbb5e59b878234fe7bd563870d0bd9
ew-aolcc <bizjay61@gmail.com>
2026-08-13 15:01:59 -0400

    Basic HTML and JavaScript 1 Assignment

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+// alert("Hello World!");
+
+function My_First_Function() {
+    var String = "Hello World!";
+    var result = String.fontcolor('green')
+    document.getElementById('Irish').innerHTML = result
+}
